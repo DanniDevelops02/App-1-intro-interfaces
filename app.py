1241c3b98a833a2aba7d313ccd4f3ad7283d6e0d
DanniDevelops02 <daniel.florez.puentes@gmail.com>
2026-08-20 10:00:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Hola!!! mi Nombre es Danniboy")
+st.header("En este espacio cmoienzo a desarrollar mis aplicaciones para interfaces multimodales.")
