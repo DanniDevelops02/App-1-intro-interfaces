f5d70bb574cbc062b24a235520298aa95314c7e9
DanniDevelops02 <daniel.florez.puentes@gmail.com>
2026-08-20 10:04:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 from PIL import Image
 
 st.title("Hola!!! mi Nombre es Danniboy")
+
 st.header("En este espacio cmoienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open("sergey-kolesov-mermaid-sharpen.jpg")
+st.image(image, caption = "interfaces multimodales")
+
+
